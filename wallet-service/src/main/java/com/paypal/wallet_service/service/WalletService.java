b6ce157267300b958f5251c1da356cee374e4746
Fallen-Angel61 <debarshigupta47@gmail.com>
2026-05-16 07:48:35 +0530

    Refactor code

--- wallet-service/src/main/java/com/paypal/wallet_service/service/WalletService.java
+++ wallet-service/src/main/java/com/paypal/wallet_service/service/WalletService.java
@@ -4,11 +4,7 @@ import com.paypal.wallet_service.kafka.events.DepositInitiatedEvent;
 import com.paypal.wallet_service.kafka.events.DepositSucceededEvent;
 import com.paypal.wallet_service.kafka.events.TransferInitiatedEvent;
 import com.paypal.wallet_service.kafka.events.TransferSucceededEvent;
-import com.paypal.wallet_service.model.dto.DepositResponse;
 import com.paypal.wallet_service.model.dto.UserBalanceResponse;
-import com.paypal.wallet_service.model.entity.Wallet;
-
-import java.math.BigDecimal;
 
 public interface WalletService {
 
